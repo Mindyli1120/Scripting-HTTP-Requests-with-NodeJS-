9b19778afd591fb751ffb2559b9b7488a5a23fb7
mindyli1120 <mindyli1989@gmail.com>
2018-06-11 19:54:02 +0000

    step 5

---
+++ http-functions.js
@@ -0,0 +1,15 @@
+var https = require('https');
+
+module.exports = function getHTML (options, callback) {
+    https.get(options, function (response) {
+        response.setEncoding('utf8');
+        var output = "";
+        response.on('data', function (data) {
+            output += data;
+        });
+
+        response.on('end', function() {
+            callback(output)
+        });
+    });
+};
